386803fd23a71f2a6b7f9b380c4085cfb20e4756
Yonatan Keren <yonatan.keren1@gmail.com>
2025-03-18 22:15:17 +0200

    Change onBallClick and HTML title

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict'
 
-function onBallClick(){
-    console.log('Hi')
+
+function onBallClick(elBall){
+    var currWidth = elBall.offsetWidth
+    var currHeight = elBall.offsetHeight
+    elBall.style.width = (currWidth + 50) + 'px'
+    elBall.style.height = (currHeight + 50) + 'px'
+    elBall.textContent = currHeight + 50
 }
