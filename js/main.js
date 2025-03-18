28934040e9962ddb084fa6fc594964be1ba108ee
Yonatan Keren <yonatan.keren1@gmail.com>
2025-03-18 22:29:11 +0200

    Add transition consideration in function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 'use strict'
 
 
-function onBallClick(elBall){
+function onBallClick(elBall) {
+    elBall.style.transition = 'none'
     var currWidth = elBall.offsetWidth
     var currHeight = elBall.offsetHeight
-    elBall.style.width = (currWidth + 50) + 'px'
-    elBall.style.height = (currHeight + 50) + 'px'
-    elBall.textContent = currHeight + 50
+    if (currHeight >= 400 || currWidth >= 400) {
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+        elBall.textContent = 100
+    } else {
+        elBall.style.width = (currWidth + 50) + 'px'
+        elBall.style.height = (currHeight + 50) + 'px'
+        elBall.textContent = currHeight + 50
+    }
+    elBall.style.transition = '1s'
 }
