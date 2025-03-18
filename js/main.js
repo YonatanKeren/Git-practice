45c00d1d173fd957baf2919ad7488a499bb55bfc
Yonatan Keren <yonatan.keren1@gmail.com>
2025-03-18 22:51:27 +0200

    Change onBallClick

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@
 
 function onBallClick(elBall) {
     elBall.style.transition = 'none'
-    var currWidth = elBall.offsetWidth
-    var currHeight = elBall.offsetHeight
+    var increment = getRndIntIncMax(20, 60)
+    console.log(increment)
+    var currWidth = elBall.offsetWidth + increment
+    var currHeight = elBall.offsetHeight + increment
     if (currHeight >= 400 || currWidth >= 400) {
         elBall.style.width = '100px'
         elBall.style.height = '100px'
         elBall.textContent = 100
     } else {
-        elBall.style.width = (currWidth + 50) + 'px'
-        elBall.style.height = (currHeight + 50) + 'px'
-        elBall.textContent = currHeight + 50
+        elBall.style.width = currWidth + 'px'
+        elBall.style.height = currHeight + 'px'
+        elBall.textContent = currHeight
     }
     elBall.style.transition = '1s'
 }
